354214dcfe53cd8c90a7f54abacbc6df70b0f8f9
Alejandro Seguí <alesegdia@gmail.com>
2015-04-22 05:57:43 +0200

    Parsing tree.

--- Session.py
+++ Session.py
@@ -5,44 +5,54 @@ import sys
 import gtk
 import terminatorlib.plugin as plugin
 from terminatorlib.translation import _
+import pprint
 
 
 # AVAILABLE must contain a list of all the classes that you want exposed
 AVAILABLE = ['Session']
 
 class Session(plugin.MenuItem):
-    capabilities = ['terminal_menu']
-    dialog_action = gtk.FILE_CHOOSER_ACTION_SAVE
-    dialog_buttons = (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
-                      gtk.STOCK_SAVE, gtk.RESPONSE_OK)
-    def __init__(self):
-        plugin.MenuItem.__init__(self)
+	capabilities = ['terminal_menu']
+	dialog_action = gtk.FILE_CHOOSER_ACTION_SAVE
+	dialog_buttons = (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
+			gtk.STOCK_SAVE, gtk.RESPONSE_OK)
+	def __init__(self):
+		plugin.MenuItem.__init__(self)
 
-    def callback(self, menuitems, menu, terminal):
-    	item = gtk.MenuItem(_("Save Session"))
-    	submenu = gtk.Menu()
-    	saveItem = gtk.MenuItem("Save")
-    	saveItem.connect("activate", self.save_session, terminal)
-    	loadItem = gtk.MenuItem("Load")
-    	loadItem.connect("activate", self.load_session, terminal)
-    	debugTermsItem = gtk.MenuItem("Debug terminals")
-    	debugTermsItem.connect("activate", self.debug_terminals, terminal)
-    	spawnItem = gtk.MenuItem("Recursive spawn")
-    	spawnItem.connect("activate", self.recursive_spawn, terminal)
-    	submenu.append(saveItem)
-    	submenu.append(loadItem)
-    	submenu.append(debugTermsItem)
-    	submenu.append(spawnItem)
-    	item.set_submenu(submenu)
-    	menuitems.append(item)
+	def callback(self, menuitems, menu, terminal):
+		item = gtk.MenuItem(_("Save Session"))
+		submenu = gtk.Menu()
+		saveItem = gtk.MenuItem("Save")
+		saveItem.connect("activate", self.save_session, terminal)
+		loadItem = gtk.MenuItem("Load")
+		loadItem.connect("activate", self.load_session, terminal)
+		debugTermsItem = gtk.MenuItem("Debug terminals")
+		debugTermsItem.connect("activate", self.debug_terminals, terminal)
+		spawnItem = gtk.MenuItem("Recursive spawn")
+		spawnItem.connect("activate", self.recursive_spawn, terminal)
+		submenu.append(saveItem)
+		submenu.append(loadItem)
+		submenu.append(debugTermsItem)
+		submenu.append(spawnItem)
+		item.set_submenu(submenu)
+		menuitems.append(item)
 
-    def load_session(self, _widget, Terminal):
-    	pass
+	def load_session(self, _widget, Terminal):
+		pass
 
-    def save_session(self, _widget, Terminal):
-    	pass
+	def recursive_save(self, node):
+		class_string = node.__class__.__name__
+		if class_string == "VPaned" or class_string == "HPaned":
+			return { "node_type": class_string, "children": [self.recursive_save(child) for child in node.get_children()] }
+		else:
+			return { "node_type": class_string, "element": node }
 
-    def debug_terminals(self, _widget, Terminal):
+	def save_session(self, _widget, Terminal):
+		win = Terminal.terminator.windows[0]
+		open_list = win.get_children()
+		pprint.pprint(self.recursive_save(win.get_children()[0]))
+
+	def debug_terminals(self, _widget, Terminal):
 		for terminal in Terminal.terminator.terminals:
 			print("title", terminal.titlebar.label._label.get_text())
 			print("size", terminal.get_size())
@@ -50,7 +60,7 @@ class Session(plugin.MenuItem):
 			print("position", terminal.window.get_position())
 			print("=========================")
 
-    def recursive_spawn(self, _widget, Terminal):
+	def recursive_spawn(self, _widget, Terminal):
 		win = Terminal.terminator.windows[0]
 		open_list = win.get_children()
 		while len(open_list) > 0:
